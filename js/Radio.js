8a61f302d5b0d4a9e0011d043a225b6ee6468598
MUHAMMED YAYLA <muhammeddyayla@gmail.com>
2023-05-18 16:03:39 +0300

    Add more buttons like next previous

--- js/Radio.js
+++ js/Radio.js
@@ -4,16 +4,16 @@ class Radio {
 		this.playlist = [
 			{name: 'Nightwave Plaza', slug: 'nightwave', src: 'https://radio.plaza.one/mp3'},
 			{name: 'Classic Rock Florida HD', slug: 'classicrockflorida', src: 'https://vip2.fastcast4u.com/proxy/classicrockdoug?mp=/1'},
-			{name: 'Baba Radyo', slug: 'babaradyo', src: 'http://37.247.98.7/babaradyomp3'},
-			{name: 'Best Fm', slug: 'bestfm', src: 'http://46.20.7.126/;stream.mp3'},
-			{name: 'Pal Fm', slug: 'palfm', src: 'http://shoutcast.radyogrup.com:1030/;'},
 			{name: 'Radyo Arabesk', slug: 'radyoarabesk', src: 'https://yayin.radyoarabesk.com.tr:8000/stream'},
 			{name: 'Gold FM', slug: 'gold', src: 'http://dijimedya.radyotvonline.net/goldfm'},
-			{name: 'Kent Fm', slug: 'kentfm', src: 'https://anadolu.liderhost.com.tr:10906/;'},
+			{name: 'Radyo Kent', slug: 'kent', src: 'https://radyo1.radyo-dinle.tc:7060/;'},
 			{name: 'Dost FM', slug: 'Dost', src: ' http://yayin.dostfm.com:8920/;'},
 			{name: 'Doksanlar', slug: 'doksanlar', src: 'http://37.247.98.8/stream/166/'},
-			{name: 'KRAL DAMAR FM', slug: 'kraldamar', src: 'https://17733.live.streamtheworld.com/KISS_FM128AAC.aac'},
-			{name: 'Eksen', slug: 'eksen', src: 'http://eksenwmp.radyotvonline.com/;'}
+			{name: 'Zen Nostalji', slug: 'zennostalji', src: 'http://37.59.205.232:7794/;'},
+			{name: 'Eksen', slug: 'eksen', src: 'http://eksenwmp.radyotvonline.com/;'},
+			{name: 'Baba Radyo', slug: 'babaradyo', src: 'http://37.247.98.7/babaradyomp3'},
+			{name: 'Best Fm', slug: 'bestfm', src: 'http://46.20.7.126/;stream.mp3'},
+			{name: 'Pal Fm', slug: 'palfm', src: 'http://shoutcast.radyogrup.com:1030/;'}
 		];
 		this.player.src = this.playlist[0].src;
 		this.player.preload = 'auto';
--- js/main.js
+++ js/main.js
@@ -43,33 +43,95 @@ function init(){
 	update();
 }
 
-function createUI(){
-	pause = document.createElement('div');
-	pause.id = 'pause';
-	let select = document.createElement('select');
-	select.id = 'playlist';
-	let slug = getParameter();
-	for(let i = 0; i < radio.playlist.length; i++){
-		let opt = document.createElement('option');
-		opt.value = i;
-		opt.innerText = radio.playlist[i].name;
-		if( radio.playlist[i].slug === slug){
-			opt.selected = true;
-			radio.player.src = radio.playlist[i].src;
-		}
-		select.appendChild( opt );
-	}
-	select.addEventListener('change',function(){
-		radio.player.src = radio.playlist[this.value].src;
-	});
-	let btn = document.createElement('button');
-	btn.id = 'btnPlay';
-	btn.innerText = "Play";
-	btn.addEventListener('click', (e)=>{ togglePlay(e) } );
-	pause.appendChild( select );
-	pause.appendChild( btn );
-	document.body.appendChild( pause );
+function createUI() {
+    pause = document.createElement('div');
+    pause.id = 'pause';
+    let select = document.createElement('select');
+    select.id = 'playlist';
+    let slug = getParameter();
+    for (let i = 0; i < radio.playlist.length; i++) {
+        let opt = document.createElement('option');
+        opt.value = i;
+        opt.innerText = radio.playlist[i].name;
+        if (radio.playlist[i].slug === slug) {
+            opt.selected = true;
+            radio.player.src = radio.playlist[i].src;
+        }
+        select.appendChild(opt);
+    }
+    select.addEventListener('change', function () {
+        radio.player.src = radio.playlist[this.value].src;
+    });
+
+    let btnPrev = document.createElement('button');
+    btnPrev.id = 'btnPrev';
+    btnPrev.innerText = 'Previous';
+    btnPrev.addEventListener('click', () => { playPrevious() });
+    btnPrev.style.marginRight = '10px'; // Add margin-right for spacing
+    btnPrev.style.backgroundColor = 'lightgray'; // Set background color
+    btnPrev.style.height = '30px'; // Set height
+    btnPrev.style.width = '80px'; // Set width
+    btnPrev.style.border = 'none'; // Remove border
+    btnPrev.style.borderRadius = '4px'; // Add border radius
+    btnPrev.style.cursor = 'pointer'; // Add cursor style
+
+    let btnNext = document.createElement('button');
+    btnNext.id = 'btnNext';
+    btnNext.innerText = 'Next';
+    btnNext.addEventListener('click', () => { playNext() });
+    btnNext.style.marginRight = '10px'; // Add margin-right for spacing
+    btnNext.style.backgroundColor = 'lightgray'; // Set background color
+    btnNext.style.height = '30px'; // Set height
+    btnNext.style.width = '80px'; // Set width
+    btnNext.style.border = 'none'; // Remove border
+    btnNext.style.borderRadius = '4px'; // Add border radius
+    btnNext.style.cursor = 'pointer'; // Add cursor style
+
+    let btnPlay = document.createElement('button');
+    btnPlay.id = 'btnPlay';
+    btnPlay.innerText = 'Play';
+    btnPlay.addEventListener('click', (e) => { togglePlay(e) });
+    btnPlay.style.backgroundColor = 'lightgray'; // Set background color
+    btnPlay.style.height = '30px'; // Set height
+    btnPlay.style.width = '80px'; // Set width
+    btnPlay.style.border = 'none'; // Remove border
+    btnPlay.style.borderRadius = '4px'; // Add border radius
+    btnPlay.style.cursor = 'pointer'; // Add cursor style
+	let volumeBar = document.createElement('input');
+    volumeBar.type = 'range';
+    volumeBar.id = 'volumeBar';
+    volumeBar.min = '0';
+    volumeBar.max = '1';
+    volumeBar.step = '0.01';
+    volumeBar.value = '1';
+    volumeBar.addEventListener('input', (e) => { adjustVolume(e.target.value) });
+
+    pause.appendChild(select);
+    pause.appendChild(btnPrev);
+    pause.appendChild(btnNext);
+    pause.appendChild(btnPlay);
+	pause.appendChild(volumeBar);
+    document.body.appendChild(pause);
 }
+
+function adjustVolume(volume) {
+    radio.player.volume = volume;
+}
+
+function playPrevious() {
+    let currentIndex = parseInt(document.getElementById('playlist').value);
+    let previousIndex = (currentIndex - 1 + radio.playlist.length) % radio.playlist.length;
+    document.getElementById('playlist').value = previousIndex;
+    radio.player.src = radio.playlist[previousIndex].src;
+}
+
+function playNext() {
+    let currentIndex = parseInt(document.getElementById('playlist').value);
+    let nextIndex = (currentIndex + 1) % radio.playlist.length;
+    document.getElementById('playlist').value = nextIndex;
+    radio.player.src = radio.playlist[nextIndex].src;
+}
+
 
 function selectAnimation(){
 	time = (Math.random() * 7000) + 7000;
